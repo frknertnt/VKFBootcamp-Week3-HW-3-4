c04f95f8a02de0f777cd40838599f2391a72c834
Furkan Ertantu <furkanertantu@outlook.com>
2023-09-24 18:17:40 +0300

    connect database with inmemory data

---
+++ WebApi/DBOperations/BookStoreDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Entities;
+
+namespace WebApi.DBOperations
+{
+    public class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Book> Books { get; set; }
+
+        public override int SaveChanges()
+        {
+            return base.SaveChanges();
+        }
+    }
+}
