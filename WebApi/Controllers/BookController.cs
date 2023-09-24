da11bf94fcd2cef5832adb024a397d4cf4b31a4d
Furkan Ertantu <furkanertantu@outlook.com>
2023-09-24 18:16:32 +0300

    api controller

---
+++ WebApi/Controllers/BookController.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using WebApi.Application.BookOperations.CreateBook;
+using WebApi.Application.BookOperations.DeleteBook;
+using WebApi.Application.BookOperations.GetBookDetail;
+using WebApi.Application.BookOperations.GetBooks;
+using WebApi.Application.BookOperations.UpdateBook;
+using WebApi.DBOperations;
+using WebApi.Entities;
+using static WebApi.Application.BookOperations.CreateBook.CreateBookCommand;
+using static WebApi.Application.BookOperations.UpdateBook.UpdateBookCommand;
+using ValidationResult = FluentValidation.Results.ValidationResult;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly BookStoreDbContext _context;
+        private readonly IMapper _mapper;
+        public BookController(BookStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            GetBooksQuery query = new GetBooksQuery(_context, _mapper);
+            var result = query.Handle();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            BookDetailViewModel viewModel;
+
+            GetBookDetailQuery detail = new GetBookDetailQuery(_context, _mapper);
+            detail.BookId = id;
+            GetBookDetailQueryValidator validator = new GetBookDetailQueryValidator();
+            validator.ValidateAndThrow(detail);
+            viewModel = detail.Handle();
+
+            return Ok(viewModel);
+
+        }
+
+        [HttpPost]
+        public IActionResult AddBook([FromBody] CreateBookModel newBook)
+        {
+            CreateBookCommand command = new CreateBookCommand(_context, _mapper);
+
+            command.Model = newBook;
+            CreateBookCommandValidator validator = new CreateBookCommandValidator();
+            validator.ValidateAndThrow(command);
+            command.Handle();
+
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] UpdateBookModel updateBook)
+        {
+            UpdateBookCommand command = new UpdateBookCommand(_context);
+            command.BookId = id;
+            command.Model = updateBook;
+            UpdateBookCommandValidator validator = new UpdateBookCommandValidator();
+            validator.ValidateAndThrow(command);
+            command.Handle();
+
+            return Ok();
+
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteBook(int id)
+        {
+            DeleteBookCommand command = new DeleteBookCommand(_context);
+            command.BookId = id;
+            DeleteBookCommandValidator validationRules = new DeleteBookCommandValidator();
+            validationRules.ValidateAndThrow(command);
+            command.Handle();
+
+            return Ok();
+
+        }
+    }
+}
