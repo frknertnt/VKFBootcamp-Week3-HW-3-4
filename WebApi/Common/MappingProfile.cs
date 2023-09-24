a04f9160af7eacc0f1c711fef38bfd8f8d8ce226
Furkan Ertantu <furkanertantu@outlook.com>
2023-09-24 18:16:13 +0300

    using AutoMapper

---
+++ WebApi/Common/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using WebApi.Application.BookOperations.CreateBook;
+using WebApi.Application.BookOperations.GetBookDetail;
+using WebApi.Application.BookOperations.GetBooks;
+using WebApi.Entities;
+using static WebApi.Application.BookOperations.CreateBook.CreateBookCommand;
+
+namespace WebApi.Common
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CreateBookModel, Book>();
+            CreateMap<Book, BookDetailViewModel>().ForMember(destination => destination.Genre, opt => opt.MapFrom(src => ((GenreEnum)src.GenreId).ToString()));
+            CreateMap<Book, BookViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => ((GenreEnum)src.GenreId).ToString()));
+        }
+    }
+}
